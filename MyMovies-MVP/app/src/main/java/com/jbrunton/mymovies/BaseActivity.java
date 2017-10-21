201f8a8b5d1ac51fe916bc4297a5b52aa3cc7821
John Brunton <jbrunton@zipcar.com>
2017-10-21 15:40:29 +0100

    move SearchResultsAdapter

--- MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/BaseActivity.java
+++ MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.jbrunton.mymovies;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableTransformer;
@@ -11,5 +12,9 @@ public class BaseActivity extends AppCompatActivity {
     protected <T> ObservableTransformer<T, T> applySchedulers() {
         return observable -> observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    protected int toVisibility(boolean show) {
+        return show ? View.VISIBLE : View.GONE;
     }
 }
--- MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/MainActivity.java
+++ MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/MainActivity.java
@@ -1,35 +1,28 @@
 package com.jbrunton.mymovies;
 
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.jbrunton.mymovies.search.SearchItemViewState;
+import com.jbrunton.mymovies.search.SearchResultsAdapter;
 import com.jbrunton.mymovies.search.SearchViewModel;
 import com.jbrunton.mymovies.search.SearchViewState;
-import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
-import static android.text.Html.FROM_HTML_MODE_COMPACT;
-
 public class MainActivity extends BaseActivity {
     private RecyclerView moviesList;
-    private MoviesAdapter moviesAdapter;
+    private SearchResultsAdapter searchResultsAdapter;
     @BindView(R.id.search_query) EditText searchQuery;
     @BindView(R.id.error_case) View errorCase;
     @BindView(R.id.error_text) TextView errorText;
@@ -51,8 +44,8 @@ public class MainActivity extends BaseActivity {
         //moviesList.setLayoutManager(new GridLayoutManager(this, 2));
         moviesList.setLayoutManager(new LinearLayoutManager(this));
 
-        moviesAdapter = new MoviesAdapter(this);
-        moviesList.setAdapter(moviesAdapter);
+        searchResultsAdapter = new SearchResultsAdapter(this);
+        moviesList.setAdapter(searchResultsAdapter);
 
         viewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         viewModel.viewState().observe(this, this::updateView);
@@ -74,7 +67,7 @@ public class MainActivity extends BaseActivity {
 
     private void updateView(SearchViewState viewState) {
         moviesList.setVisibility(toVisibility(!viewState.showError()));
-        moviesAdapter.setDataSource(viewState.movies());
+        searchResultsAdapter.setDataSource(viewState.movies());
 
         errorCase.setVisibility(toVisibility(viewState.showError()));
         errorText.setText(viewState.errorMessage());
@@ -82,53 +75,4 @@ public class MainActivity extends BaseActivity {
         errorTryAgainButton.setVisibility(toVisibility(viewState.showTryAgainButton()));
     }
 
-    private int toVisibility(boolean show) {
-        return show ? View.VISIBLE : View.GONE;
-    }
-
-    private static class MoviesAdapter extends BaseRecyclerAdapter<SearchItemViewState, MoviesAdapter.ViewHolder> {
-        MoviesAdapter(Context context) {
-            super(R.layout.item_movie_card, new ViewHolderFactory(context));
-        }
-
-        static class ViewHolder extends RecyclerView.ViewHolder {
-            private TextView titleView;
-            private TextView releaseDateView;
-            private ImageView poster;
-            private TextView ratingView;
-
-            ViewHolder(View itemView) {
-                super(itemView);
-                titleView = (TextView) itemView.findViewById(R.id.title);
-                releaseDateView = (TextView) itemView.findViewById(R.id.release_date);
-                ratingView = (TextView) itemView.findViewById(R.id.rating);
-                poster = (ImageView) itemView.findViewById(R.id.poster);
-            }
-        }
-
-        protected static class ViewHolderFactory implements BaseRecyclerAdapter.ViewHolderFactory<SearchItemViewState, ViewHolder> {
-            private final Context context;
-
-            public ViewHolderFactory(Context context) {
-                this.context = context;
-            }
-
-            @Override
-            public ViewHolder createViewHolder(View view) {
-                return new ViewHolder(view);
-            }
-
-            @Override
-            public void bindHolder(ViewHolder holder, SearchItemViewState item) {
-                holder.titleView.setText(item.title());
-                holder.releaseDateView.setText(item.yearReleased());
-                holder.ratingView.setText(Html.fromHtml(item.rating()));
-                Picasso.with(context)
-                        .load(item.posterUrl())
-                        .resize(185, 275)
-                        .centerCrop()
-                        .into(holder.poster);
-            }
-        }
-    }
 }
