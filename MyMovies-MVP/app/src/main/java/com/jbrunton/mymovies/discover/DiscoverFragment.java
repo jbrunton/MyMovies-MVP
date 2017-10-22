d16931b94535df4959035865ed3fc092970c66bd
John Brunton <jbrunton@zipcar.com>
2017-10-22 18:25:06 +0100

    use loading state in discover fragment

--- MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/discover/DiscoverFragment.java
+++ MyMovies-MVP/app/src/main/java/com/jbrunton/mymovies/discover/DiscoverFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jbrunton.mymovies.LoadingStateContext;
 import com.jbrunton.mymovies.R;
 import com.jbrunton.mymovies.search.SearchResultsAdapter;
 import com.jbrunton.mymovies.search.SearchViewModel;
@@ -23,12 +24,15 @@ import butterknife.OnClick;
 public class DiscoverFragment extends Fragment {
     private SearchResultsAdapter nowPlayingAdapter;
     private DiscoverViewModel viewModel;
+    @BindView(R.id.discover_content) View discoverContent;
     @BindView(R.id.now_playing) RecyclerView nowPlayingList;
+    private LoadingStateContext loadingStateContext = new LoadingStateContext();
 
     @Nullable @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
         ButterKnife.bind(this, view);
+        ButterKnife.bind(loadingStateContext, view);
 
         nowPlayingList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
         nowPlayingAdapter = new SearchResultsAdapter(getActivity(), R.layout.item_movie_card_grid);
@@ -51,13 +55,9 @@ public class DiscoverFragment extends Fragment {
     }
 
     private void updateView(SearchViewState viewState) {
-        nowPlayingList.setVisibility(toVisibility(!viewState.showError()));
+        discoverContent.setVisibility(toVisibility(viewState.showContent()));
         nowPlayingAdapter.setDataSource(viewState.movies());
-
-//        errorCase.setVisibility(toVisibility(viewState.showError()));
-//        errorText.setText(viewState.errorMessage());
-//        errorImage.setImageResource(viewState.errorIcon());
-//        errorTryAgainButton.setVisibility(toVisibility(viewState.showTryAgainButton()));
+        loadingStateContext.updateView(viewState);
     }
 
     protected int toVisibility(boolean show) {
