a8662e05d47e1fba24dd13cd23ee399bf263a1f7
John Brunton <jbrunton@zipcar.com>
2017-11-18 13:52:38 +0000

    add simple activity test

--- app/src/androidTest/java/com/jbrunton/mymovies/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jbrunton/mymovies/ExampleInstrumentedTest.java
@@ -1,13 +1,18 @@
 package com.jbrunton.mymovies;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.jbrunton.mymovies.app.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +21,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.jbrunton.mymovies", appContext.getPackageName());
+        onView(withId(R.id.navigation_search)).check(matches(isDisplayed()));
     }
 }
