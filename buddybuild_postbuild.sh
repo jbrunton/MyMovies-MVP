b5d1ce0ec694174d564846a6534172ca44eaa659
John Brunton <jbrunton@zipcar.com>
2017-11-19 22:07:42 +0000

    add roadtest to run on master

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -5,10 +5,19 @@ set -e
 gcloud config set project "$GCLOUD_PROJECT"
 gcloud auth activate-service-account --key-file ${BUDDYBUILD_SECURE_FILES}/gcloudkey.json "$GCLOUD_USER"
 
-./gradlew --no-daemon ":app:assembleDebugAndroidTest" ":app:assembleDebug"
-
-gcloud firebase test android run \
-      --type instrumentation \
+echo "Starting smoke test..."
+gcloud firebase test android run firebase-test-matrices.yml:smoketest \
+    --type instrumentation \
       --app $BUDDYBUILD_WORKSPACE/app/build/outputs/apk/debug/app-debug.apk \
       --test $BUDDYBUILD_WORKSPACE/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-      --device model=Nexus6,version=25,locale=en,orientation=portrait
+
+if [ "$BUDDYBUILD_BRANCH" = "master" ]
+then
+    echo "Starting road test..."
+    gcloud firebase test android run firebase-test-matrices.yml:roadtest \
+        --type instrumentation \
+          --app $BUDDYBUILD_WORKSPACE/app/build/outputs/apk/debug/app-debug.apk \
+          --test $BUDDYBUILD_WORKSPACE/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+else
+    echo "Not on master, skipping road test."
+fi
