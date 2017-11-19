c267845ab9f9e8334d54ac47e617c60b830e54d5
John Brunton <jbrunton@zipcar.com>
2017-11-19 16:49:47 +0000

    add postclone step

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# install gcloud sdk
+curl -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-180.0.0-darwin-x86_64.tar.gz | tar xz
+
+# add 'gcloud' to path
+export PATH="`pwd`/google-cloud-sdk/bin:$PATH"
+
+# authenticate
+echo $GCLOUD_KEY | base64 --decode > "gcloudkey.json"
+gcloud config set project "$GCLOUD_PROJECT"
+gcloud auth activate-service-account --key-file "gcloudkey.json" "$GCLOUD_USER"
