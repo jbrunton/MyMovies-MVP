c6e81e95d5b4dae3b1276d4899461ce40096a8e5
John Brunton <jbrunton@zipcar.com>
2017-11-21 22:02:43 +0000

    add results test

--- app/src/androidTest/java/com/jbrunton/mymovies/MainActivityTest.java
+++ app/src/androidTest/java/com/jbrunton/mymovies/MainActivityTest.java
@@ -3,6 +3,8 @@ package com.jbrunton.mymovies;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.jbrunton.entities.Movie;
+import com.jbrunton.fixtures.MovieFactory;
 import com.jbrunton.mymovies.api.DescriptiveError;
 import com.jbrunton.mymovies.app.MainActivity;
 import com.jbrunton.mymovies.app.search.SearchFragment;
@@ -15,14 +17,21 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.jbrunton.mymovies.fixtures.RecyclerViewUtils.withRecyclerView;
+import static java.util.Arrays.asList;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends BaseTest<MainActivity> {
     private static final SearchViewStateFactory FACTORY = new SearchViewStateFactory();
+
+    private static final MovieFactory MOVIE_FACTORY = new MovieFactory();
+    private static final Movie MOVIE1 = MOVIE_FACTORY.create();
+    private static final Movie MOVIE2 = MOVIE_FACTORY.create();
 
     private static final SearchViewState EMPTY_STATE = FACTORY.searchEmptyState();
     private static final SearchViewState LOADING_STATE = FACTORY.loadingState();
@@ -59,6 +68,17 @@ public class MainActivityTest extends BaseTest<MainActivity> {
                 .check(matches(withText(NETWORK_ERROR.getMessage())));
         onView(withId(R.id.error_try_again))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test public void showsResults() {
+        setViewState(FACTORY.fromList(asList(MOVIE1, MOVIE2)));
+
+        takeScreenshot();
+
+        onView(withRecyclerView(R.id.movies_list).atPosition(0))
+                .check(matches(hasDescendant(withText(MOVIE1.title()))));
+        onView(withRecyclerView(R.id.movies_list).atPosition(1))
+                .check(matches(hasDescendant(withText(MOVIE2.title()))));
     }
 
     @Override protected ActivityTestRule<MainActivity> createActivityTestRule() {
--- fixtures/src/main/java/com/jbrunton/fixtures/MovieFactory.java
+++ fixtures/src/main/java/com/jbrunton/fixtures/MovieFactory.java
@@ -14,8 +14,8 @@ public class MovieFactory {
                 .title("Movie " + id)
                 .overview(Optional.of("Overview for Movie " + id))
                 .releaseDate(Optional.empty())
-                .posterUrl(Optional.empty())
-                .backdropUrl(Optional.empty())
+                .posterUrl(Optional.of("https://image.tmdb.org/t/p/w300/bIuOWTtyFPjsFDevqvF3QrD1aun.jpg"))
+                .backdropUrl(Optional.of("https://image.tmdb.org/t/p/w300/LvmmDZxkTDqp0DX7mUo621ahdX.jpg"))
                 .rating("");
     }
 
