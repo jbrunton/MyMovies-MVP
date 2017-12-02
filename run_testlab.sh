b9cf5a79abe49c8b2acaf5de873f58384b8e1471
John Brunton <jbrunton@zipcar.com>
2017-12-02 16:31:07 +0000

    hardcore location of gcloud for now

--- run_testlab.sh
+++ run_testlab.sh
@@ -4,10 +4,10 @@ set -e
 
 ./gradlew assembleDebugAndroidTest
 
-gcloud config set project mymovies-7e138
-gcloud auth activate-service-account --key-file ${JENKINS_HOME}/gcloudkey.json mymovies-7e138@appspot.gserviceaccount.com
+/var/jenkins_home/gcloud/google-cloud-sdk/bin/gcloud config set project mymovies-7e138
+/var/jenkins_home/gcloud/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${JENKINS_HOME}/gcloudkey.json mymovies-7e138@appspot.gserviceaccount.com
 
-gcloud firebase test android run firebase-test-matrices.yml:smoketest \
+/var/jenkins_home/gcloud/google-cloud-sdk/bin/gcloud firebase test android run firebase-test-matrices.yml:smoketest \
     --type instrumentation \
       --app ./app/build/outputs/apk/debug/app-debug.apk \
       --test ./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
