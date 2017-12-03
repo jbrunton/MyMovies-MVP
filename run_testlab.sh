1c9ca329f899cdaa5251380fc5f456954f9693f8
John Brunton <jbrunton@zipcar.com>
2017-12-03 11:37:02 +0000

    run road tests on master

--- run_testlab.sh
+++ run_testlab.sh
@@ -7,7 +7,19 @@ set -e
 gcloud config set project mymovies-7e138
 gcloud auth activate-service-account --key-file ${JENKINS_HOME}/gcloudkey.json mymovies-7e138@appspot.gserviceaccount.com
 
+echo "Running smoke tests..."
 gcloud firebase test android run firebase-test-matrices.yml:smoketest \
     --type instrumentation \
       --app ./app/build/outputs/apk/debug/app-debug.apk \
       --test ./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+
+if [ $BRANCH_NAME = "master" ];
+then
+    echo "On master. Running road tests..."
+    gcloud firebase test android run firebase-test-matrices.yml:roadtest \
+        --type instrumentation \
+          --app ./app/build/outputs/apk/debug/app-debug.apk \
+          --test ./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+else
+    echo "Not on master. Skipping road tests."
+fi
