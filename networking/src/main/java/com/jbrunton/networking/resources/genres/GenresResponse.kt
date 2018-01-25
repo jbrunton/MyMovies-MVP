8ab11a4c6bf3e45c7353e682efffc4f7cbc36b9a
John Brunton <jbrunton@zipcar.com>
2018-01-25 09:36:42 +0000

    implement GenresResponse as data class

--- networking/src/main/java/com/jbrunton/networking/resources/genres/GenresResponse.kt
+++ networking/src/main/java/com/jbrunton/networking/resources/genres/GenresResponse.kt
@@ -2,19 +2,15 @@ package com.jbrunton.networking.resources.genres
 
 import com.jbrunton.entities.Genre
 
-class GenresResponse {
-    private val genres: List<GenreResource>? = null
+data class GenresResponse(private val genres: List<GenreResource>) {
 
-    private class GenreResource {
-        private val id: String? = null
-        private val name: String? = null
-
+    data class GenreResource(private val id: String, private val name: String) {
         fun toGenre(): Genre {
-            return Genre(id!!, name!!)
+            return Genre(id, name)
         }
     }
 
     fun toCollection(): List<Genre> {
-        return genres!!.map { it.toGenre() }
+        return genres.map { it.toGenre() }
     }
 }
