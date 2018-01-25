0f3428406173fb53f4dee3ea5fd420f2c8db9d71
John Brunton <jbrunton@zipcar.com>
2018-01-25 22:57:06 +0000

    write methods as single expression functions

--- networking/src/main/java/com/jbrunton/networking/resources/genres/GenresResponse.kt
+++ networking/src/main/java/com/jbrunton/networking/resources/genres/GenresResponse.kt
@@ -5,12 +5,8 @@ import com.jbrunton.entities.Genre
 data class GenresResponse(private val genres: List<GenreResource>) {
 
     data class GenreResource(private val id: String, private val name: String) {
-        fun toGenre(): Genre {
-            return Genre(id, name)
-        }
+        fun toGenre(): Genre = Genre(id, name)
     }
 
-    fun toCollection(): List<Genre> {
-        return genres.map { it.toGenre() }
-    }
+    fun toCollection(): List<Genre> = genres.map { it.toGenre() }
 }
