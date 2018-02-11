23d707c50ce8ab5415660c5f7b3009de1d7f3ce4
John Brunton <jbrunton@zipcar.com>
2018-02-11 21:14:57 +0000

    convert genre results activity and viewmodel to kotlin

---
+++ app/src/main/java/com/jbrunton/mymovies/discover/GenreResultsActivity.kt
@@ -0,0 +1,56 @@
+package com.jbrunton.mymovies.discover
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+import com.jbrunton.mymovies.R
+import com.jbrunton.mymovies.helpers.observe
+import com.jbrunton.mymovies.helpers.toVisibility
+import com.jbrunton.mymovies.search.SearchResultsAdapter
+import com.jbrunton.mymovies.search.SearchViewState
+import com.jbrunton.mymovies.shared.BaseActivity
+import kotlinx.android.synthetic.main.activity_genre_results.*
+
+class GenreResultsActivity : BaseActivity<GenreResultsViewModel>() {
+    private lateinit var moviesAdapter: SearchResultsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_genre_results)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        moviesAdapter = SearchResultsAdapter(this, R.layout.item_movie_card_list)
+        movies_list.adapter = moviesAdapter
+        movies_list.layoutManager = LinearLayoutManager(this)
+
+
+        viewModel.viewState().observe(this, this::updateView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun provideViewModel(): GenreResultsViewModel {
+        val factory = GenreResultsViewModel.Factory(
+                intent.extras!!.getString("GENRE_ID"), dependencies().moviesRepository)
+        return getViewModel(GenreResultsViewModel::class.java, factory)
+    }
+
+    private fun updateView(viewState: SearchViewState) {
+        updateLoadingView(viewState.loadingViewState)
+        movies_list.visibility = toVisibility(!viewState.loadingViewState.showError())
+        moviesAdapter!!.setDataSource(viewState.movies)
+    }
+}
