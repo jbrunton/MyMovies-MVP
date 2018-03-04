b27c43f76884601fcb36b4b12f8422710317a511
John Brunton <jbrunton@zipcar.com>
2018-03-04 17:19:15 +0000

    always source gcloud path.bash.inc

--- ci/install-gcloud.sh
+++ ci/install-gcloud.sh
@@ -7,5 +7,6 @@ if [ ! -d $HOME/google-cloud-sdk ]; then
   curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-182.0.0-linux-x86_64.tar.gz -o gcloud.tar.gz
   tar xzf gcloud.tar.gz -C $HOME
   $GCLOUD_SDK_DIR/install.sh --quiet --usage-reporting false
-  source $GCLOUD_SDK_DIR/path.bash.inc
 fi
+
+source $GCLOUD_SDK_DIR/path.bash.inc
