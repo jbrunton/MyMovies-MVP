dce9e5cee583fcf48c81f2e6af85d3125f90b35f
John Brunton <jbrunton@zipcar.com>
2018-08-16 23:06:40 +0100

    convert MovieService and HttpMoviesRepository to Kotlin

---
+++ networking/src/main/java/com/jbrunton/networking/repositories/HttpMoviesRepository.kt
@@ -0,0 +1,41 @@
+package com.jbrunton.networking.repositories
+
+import com.jbrunton.entities.Configuration
+import com.jbrunton.entities.Movie
+import com.jbrunton.entities.MoviesRepository
+import com.jbrunton.networking.resources.movies.MovieDetailsResponse
+import com.jbrunton.networking.resources.movies.MoviesCollection
+import com.jbrunton.networking.services.MovieService
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.Observables
+
+class HttpMoviesRepository(private val service: MovieService) : MoviesRepository {
+
+    override fun getMovie(movieId: String): Observable<Movie> {
+        return Observables.zip(service.movie(movieId), config()) {
+            response, config -> MovieDetailsResponse.toMovie(response, config)
+        }
+    }
+
+    override fun searchMovies(query: String): Observable<List<Movie>> {
+        return Observables.zip(service.search(query), config()) {
+            response, config -> MoviesCollection.toCollection(response, config)
+        }
+    }
+
+    override fun nowPlaying(): Observable<List<Movie>> {
+        return Observables.zip(service.nowPlaying(), config()) {
+            response, config -> MoviesCollection.toCollection(response, config)
+        }
+    }
+
+    override fun discoverByGenre(genreId: String): Observable<List<Movie>> {
+        return Observables.zip(service.discoverByGenre(genreId), config()) {
+            response, config -> MoviesCollection.toCollection(response, config)
+        }
+    }
+
+    private fun config(): Observable<Configuration> {
+        return service.configuration().map { it.toModel() }
+    }
+}
