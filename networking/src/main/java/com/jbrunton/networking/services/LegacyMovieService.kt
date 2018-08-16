1a0e7c8b4f92777f9383b32722953ca55fc63715
John Brunton <jbrunton@zipcar.com>
2018-08-16 23:06:40 +0100

    convert LegacyMovieService and HttpMoviesRepository to Kotlin

---
+++ networking/src/main/java/com/jbrunton/networking/services/LegacyMovieService.kt
@@ -0,0 +1,31 @@
+package com.jbrunton.networking.services
+
+import com.jbrunton.networking.resources.configuration.ConfigurationResponse
+import com.jbrunton.networking.resources.genres.GenresResponse
+import com.jbrunton.networking.resources.movies.MovieDetailsResponse
+import com.jbrunton.networking.resources.movies.MoviesCollection
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface LegacyMovieService {
+    @GET("configuration")
+    fun configuration(): Observable<ConfigurationResponse>
+
+    @GET("movie/{movie_id}")
+    fun movie(@Path("movie_id") movieId: String): Observable<MovieDetailsResponse>
+
+    @GET("search/movie")
+    fun search(@Query("query") query: String): Observable<MoviesCollection>
+
+    @GET("movie/now_playing")
+    fun nowPlaying(): Observable<MoviesCollection>
+
+    @GET("genre/movie/list")
+    fun genres(): Observable<GenresResponse>
+
+    @GET("discover/movie")
+    fun discoverByGenre(@Query("with_genres") genreId: String): Observable<MoviesCollection>
+}
