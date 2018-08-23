81696789322e12a7a11738b3a6db4109ebaf6ba5
John Brunton <jbrunton@zipcar.com>
2018-08-23 23:57:31 +0100

    add TestDispatchers

---
+++ app/src/test/java/com/jbrunton/mymovies/fixtures/TestDispatchers.kt
@@ -0,0 +1,6 @@
+package com.jbrunton.mymovies.fixtures
+
+import com.jbrunton.mymovies.shared.CoroutineDispatchers
+import kotlinx.coroutines.Unconfined
+
+val TestDispatchers = CoroutineDispatchers(Unconfined, Unconfined)
