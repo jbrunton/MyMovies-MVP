638bc96750bc62beafc4aa6acb34aab6cda346ad
John Brunton <jbrunton@zipcar.com>
2018-10-28 17:09:36 +0000

    use actual SearchViewModel

--- app/src/androidTest/java/com/jbrunton/mymovies/TestApplication.kt
+++ app/src/androidTest/java/com/jbrunton/mymovies/TestApplication.kt
@@ -11,21 +11,14 @@ import com.jbrunton.networking.services.ServiceFactory
 import com.nhaarman.mockito_kotlin.mock
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
-import org.koin.dsl.module.applicationContext
 import org.koin.dsl.module.module
-import kotlin.math.sin
 
 val testModule : Module = module {
     single { mock<ServiceFactory>() }
     single { mock<MoviesRepository>() }
     single { mock<GenresRepository>() }
 
-    viewModel {
-        object : SearchViewModel(get()) {
-            override fun performSearch(query: String) {}
-        } as SearchViewModel
-    }
-
+    viewModel { SearchViewModel(get()) }
     viewModel { DiscoverViewModel(get()) }
     viewModel { GenresViewModel(get()) }
     viewModel { (movieId: String) -> MovieDetailsViewModel(movieId, get()) }
