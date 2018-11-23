cee187d5663e965c8c1766cf0e9a81d184785fb6
John Brunton <jbrunton@zipcar.co.uk>
2018-11-23 20:14:25 +0000

    add tests for matching on errors

--- entities/src/test/java/com/jbrunton/entities/models/LoadingStateTest.kt
+++ entities/src/test/java/com/jbrunton/entities/models/LoadingStateTest.kt
@@ -2,6 +2,7 @@ package com.jbrunton.entities.models
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import javax.xml.ws.http.HTTPException
 
 class LoadingStateTest {
     val value = 123
@@ -10,76 +11,113 @@ class LoadingStateTest {
 
     @Test
     fun convertsSuccessToValueOrNull() {
-        assertThat(success(value).toValueOrNull()).isEqualTo(value)
+        assertThat(successState(value).toValueOrNull()).isEqualTo(value)
     }
 
     @Test
     fun convertsLoadingToValueOrNull() {
-        assertThat(loading(cachedValue).toValueOrNull()).isEqualTo(cachedValue)
-        assertThat(loading(null).toValueOrNull()).isNull()
+        assertThat(loadingState(cachedValue).toValueOrNull()).isEqualTo(cachedValue)
+        assertThat(loadingState(null).toValueOrNull()).isNull()
     }
 
     @Test
     fun convertsFailureToValueOrNull() {
-        assertThat(failure(error, cachedValue).toValueOrNull()).isEqualTo(cachedValue)
-        assertThat(failure(error,null).toValueOrNull()).isNull()
+        assertThat(failureState(error, cachedValue).toValueOrNull()).isEqualTo(cachedValue)
+        assertThat(failureState(error,null).toValueOrNull()).isNull()
     }
 
     @Test
     fun transformsSuccessfulStates() {
-        val state = success(2).map { it * 2 }
+        val state = successState(2).map { it * 2 }
         assertThat(state.toValue()).isEqualTo(4)
     }
 
     @Test
     fun transformsLoadingStates() {
-        val state = loading(2).map { it * 2 }
+        val state = loadingState(2).map { it * 2 }
         assertThat(state.toValue()).isEqualTo(4)
     }
 
     @Test
     fun transformsFailureStates() {
-        val state = failure(error, 2).map { it * 2 }
+        val state = failureState(error, 2).map { it * 2 }
         assertThat(state.toValue()).isEqualTo(4)
     }
 
     @Test
     fun transformsOnlySuccessStates() {
-        val state = success(1).onSuccess {
+        val state = successState(1).onSuccess {
             LoadingState.Success(it.value * 2)
         }
         assertThat(state).isEqualTo(LoadingState.Success(2))
     }
 
     @Test
-    fun doesNotInterceptOtherStatesWhenExpectingSuccess() {
-        loading(null).onSuccess { throw IllegalStateException() }
-        failure(error,null).onSuccess { throw IllegalStateException() }
+    fun doesNotTransformOtherStatesWhenExpectingSuccess() {
+        loadingState(null).onSuccess { throw IllegalStateException() }
+        failureState(error,null).onSuccess { throw IllegalStateException() }
     }
 
     @Test
     fun transformsOnlyLoadingStates() {
-        val state = loading(1).onLoading {
+        val state = loadingState(1).onLoading {
             LoadingState.Success(it.toValue() * 2)
         }
         assertThat(state).isEqualTo(LoadingState.Success(2))
     }
 
     @Test
-    fun doesNotInterceptOtherStatesWhenExpectingLoading() {
-        success(value).onLoading { throw IllegalStateException() }
-        failure(error,null).onLoading { throw IllegalStateException() }
+    fun doesNotTransformOtherStatesWhenExpectingLoading() {
+        successState(value).onLoading { throw IllegalStateException() }
+        failureState(error,null).onLoading { throw IllegalStateException() }
     }
 
-    private fun success(value: Int): LoadingState<Int> {
+    @Test
+    fun transformsOnlyFailureStates() {
+        val state = failureState(error,1).onLoading {
+            LoadingState.Success(it.toValue() * 2)
+        }
+        assertThat(state).isEqualTo(LoadingState.Success(2))
+    }
+
+    @Test
+    fun doesNotTransformOtherStatesWhenExpectingFailure() {
+        successState(value).onLoading { throw IllegalStateException() }
+        loadingState(value).onLoading { throw IllegalStateException() }
+    }
+
+    @Test
+    fun transformsErrors() {
+        val state = failureState(HTTPException(401), 1).onError(HTTPException::class) {
+            map { LoadingState.Success(it.toValue() * 2) }
+        }
+        assertThat(state).isEqualTo(LoadingState.Success(2))
+    }
+
+    @Test
+    fun transformsSpecificErrors() {
+        val failureState = failureState(HTTPException(401), 1)
+
+        val transformedState = failureState.onError(HTTPException::class) {
+            map { LoadingState.Success(it.toValue() * 2) } whenever { it.statusCode == 401 }
+        }
+        val otherState = failureState.onError(HTTPException::class) {
+            map { LoadingState.Success(it.toValue() * 2) } whenever { it.statusCode == 400 }
+        }
+
+        assertThat(transformedState).isEqualTo(LoadingState.Success(2))
+        assertThat(otherState).isEqualTo(failureState)
+    }
+
+    private fun successState(value: Int): LoadingState<Int> {
         return LoadingState.Success(value)
     }
 
-    private fun loading(cachedValue: Int?): LoadingState<Int> {
+    private fun loadingState(cachedValue: Int?): LoadingState<Int> {
         return LoadingState.Loading(cachedValue)
     }
 
-    private fun failure(error: Throwable, cachedValue: Int?): LoadingState<Int> {
+    private fun failureState(error: Throwable, cachedValue: Int?): LoadingState<Int> {
         return LoadingState.Failure(error, cachedValue)
     }
 }
