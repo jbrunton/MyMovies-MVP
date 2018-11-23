1b13b1e4bca7ad190016d4deb6b75a6a98ee7c0d
John Brunton <jbrunton@zipcar.com>
2018-11-23 17:48:13 +0000

    remove account code

--- entities/src/main/java/com/jbrunton/entities/repositories/AccountRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.jbrunton.entities.repositories
-
-import com.jbrunton.entities.models.Account
-import com.jbrunton.entities.models.DataStream
-
-interface AccountRepository {
-    fun account(): DataStream<Account>
-}
