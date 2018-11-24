6b4930ecae72e4279dbf47430b358d56819f1b63
John Brunton <jbrunton@zipcar.co.uk>
2018-11-24 21:56:32 +0000

    add more tests

--- app/src/androidTest/java/com/jbrunton/mymovies/moviedetails/MovieDetailsLayoutTests.kt
+++ app/src/androidTest/java/com/jbrunton/mymovies/moviedetails/MovieDetailsLayoutTests.kt
@@ -5,20 +5,21 @@ import android.widget.ProgressBar
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.jbrunton.entities.models.LoadingState
 import com.jbrunton.entities.repositories.MoviesRepository
 import com.jbrunton.fixtures.MovieFactory
 import com.jbrunton.mymovies.R
-import com.jbrunton.mymovies.fixtures.BaseActivityTest
-import com.jbrunton.mymovies.fixtures.ProgressBarViewActions
-import com.jbrunton.mymovies.fixtures.stubSearch
-import com.jbrunton.mymovies.fixtures.stubWith
+import com.jbrunton.mymovies.fixtures.*
 import com.jbrunton.mymovies.movies.MovieViewState
+import com.jbrunton.mymovies.shared.LoadingViewStateError
 import org.junit.Before
 import org.junit.Test
 import org.koin.standalone.inject
+import java.util.*
 
 class MovieDetailsLayoutTests : BaseActivityTest<MovieDetailsActivity>() {
     val LOADING_STATE = LoadingState.Loading<MovieViewState>()
@@ -26,6 +27,9 @@ class MovieDetailsLayoutTests : BaseActivityTest<MovieDetailsActivity>() {
     val movies = MovieFactory()
     val movie1 = movies.create()
     val moviesRepository: MoviesRepository by inject()
+
+    private val NETWORK_ERROR = LoadingViewStateError("Network Error", R.drawable.ic_error_outline_black_24dp, true)
+
 
     @Before
     fun setUp() {
@@ -42,8 +46,30 @@ class MovieDetailsLayoutTests : BaseActivityTest<MovieDetailsActivity>() {
         setViewState(LOADING_STATE)
 
         takeScreenshot("showsLoadingState")
-        Espresso.onView(ViewMatchers.withId(R.id.loading_indicator))
+        onView(ViewMatchers.withId(R.id.loading_indicator))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun showsErrorState() {
+        setViewState(LoadingState.Failure(NETWORK_ERROR))
+
+        takeScreenshot("showsErrorState")
+        onView(withId(R.id.error_text))
+                .check(matches(withText(NETWORK_ERROR.message)))
+        onView(withId(R.id.error_try_again))
+                .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun showsMovieDetails() {
+        val viewState = MovieViewState(movie1)
+        setViewState(LoadingState.Success(viewState))
+
+        takeScreenshot()
+
+        onView(withId(R.id.content)).check(matches(isDisplayed()))
+        onView(withId(R.id.overview)).check(matches(withText(viewState.overview)))
     }
 
     override fun createActivityTestRule(): ActivityTestRule<MovieDetailsActivity> {
