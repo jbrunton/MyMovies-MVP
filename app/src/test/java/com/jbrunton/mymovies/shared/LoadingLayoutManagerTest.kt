0948aee4ecc96fc273dc8abd2346b63d9748c654
John Brunton <jbrunton@zipcar.co.uk>
2018-11-24 15:05:56 +0000

    implement test using espresso matchers

--- app/src/test/java/com/jbrunton/mymovies/shared/LoadingLayoutManagerTest.kt
+++ app/src/test/java/com/jbrunton/mymovies/shared/LoadingLayoutManagerTest.kt
@@ -1,25 +1,22 @@
 package com.jbrunton.mymovies.shared
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import android.app.Activity
-import android.view.LayoutInflater
+import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.jbrunton.entities.models.LoadingState
-import com.jbrunton.entities.models.Movie
 import com.jbrunton.fixtures.MovieFactory
 import com.jbrunton.mymovies.R
-import com.jbrunton.mymovies.fixtures.TestActivity
-import com.jbrunton.mymovies.moviedetails.MovieDetailsViewState
 import com.jbrunton.mymovies.movies.MovieViewState
+import kotlinx.android.synthetic.main.content_movie_details.*
 import org.junit.Before
 import org.junit.Rule
-import org.robolectric.Robolectric
 
 
 
@@ -29,27 +26,35 @@ class LoadingLayoutManagerTest {
     val movies = MovieFactory()
     lateinit var view: View
     lateinit var layoutManager: LoadingLayoutManager
-    lateinit var contentView: View
 
     @get:Rule
-    val rule = ActivityTestRule(TestActivity::class.java)
+    val rule = ActivityTestRule(MyTestActivity::class.java)
 
     @Before
     fun setUp() {
-        val activity = rule.activity//Robolectric.setupActivity(TestActivity::class.java)
-        view = LayoutInflater.from(activity).inflate(R.layout.activity_movie_details, null)
-        contentView = view.findViewById(R.id.content)
-        layoutManager = LoadingLayoutManager(view, contentView)
+        view = rule.activity.findViewById(android.R.id.content)
+        layoutManager = rule.activity.layoutManager
     }
 
     @Test
     fun showsContentOnSuccessState() {
         val state = LoadingState.Success(MovieViewState(movies.create()))
         val overviewView = view.findViewById<TextView>(R.id.overview)
+
         layoutManager.updateLayout(state) {
             overviewView.text = it.overview
         }
-        assertThat(contentView.visibility).isEqualTo(View.VISIBLE)
-        assertThat(overviewView.text.toString()).isEqualTo(state.value.overview)
+
+        onView(withId(R.id.content)).check(matches(isDisplayed()))
+        onView(withId(R.id.overview)).check(matches(withText(state.value.overview)))
+    }
+
+    class MyTestActivity : BaseActivity<BaseViewModel>(){
+        lateinit var layoutManager: LoadingLayoutManager
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_movie_details)
+            layoutManager = LoadingLayoutManager.buildFor(this, content)
+        }
     }
 }
