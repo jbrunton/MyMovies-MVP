b718afa1f2d69032b8923125ced69f14c18f090f
John Brunton <jbrunton@zipcar.co.uk>
2018-11-24 14:43:45 +0000

    use ActivityTestRule

--- app/src/test/java/com/jbrunton/mymovies/shared/LoadingLayoutManagerTest.kt
+++ app/src/test/java/com/jbrunton/mymovies/shared/LoadingLayoutManagerTest.kt
@@ -8,6 +8,8 @@ import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import com.jbrunton.entities.models.LoadingState
 import com.jbrunton.entities.models.Movie
 import com.jbrunton.fixtures.MovieFactory
@@ -16,21 +18,25 @@ import com.jbrunton.mymovies.fixtures.TestActivity
 import com.jbrunton.mymovies.moviedetails.MovieDetailsViewState
 import com.jbrunton.mymovies.movies.MovieViewState
 import org.junit.Before
+import org.junit.Rule
 import org.robolectric.Robolectric
 
 
 
 
-@RunWith(RobolectricTestRunner::class)
+@RunWith(AndroidJUnit4::class)
 class LoadingLayoutManagerTest {
     val movies = MovieFactory()
     lateinit var view: View
     lateinit var layoutManager: LoadingLayoutManager
     lateinit var contentView: View
 
+    @get:Rule
+    val rule = ActivityTestRule(TestActivity::class.java)
+
     @Before
     fun setUp() {
-        val activity = Robolectric.setupActivity(TestActivity::class.java)
+        val activity = rule.activity//Robolectric.setupActivity(TestActivity::class.java)
         view = LayoutInflater.from(activity).inflate(R.layout.activity_movie_details, null)
         contentView = view.findViewById(R.id.content)
         layoutManager = LoadingLayoutManager(view, contentView)
