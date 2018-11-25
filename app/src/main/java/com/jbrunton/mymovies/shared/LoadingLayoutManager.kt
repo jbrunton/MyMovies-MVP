55df69dbb99c729b7392730e508e40943a296756
John Brunton <jbrunton@zipcar.co.uk>
2018-11-25 10:34:36 +0000

    refactor LoadingLayoutManager into distinct events

--- app/src/main/java/com/jbrunton/mymovies/shared/LoadingLayoutManager.kt
+++ app/src/main/java/com/jbrunton/mymovies/shared/LoadingLayoutManager.kt
@@ -6,7 +6,6 @@ import android.widget.TextView
 import com.jbrunton.entities.models.LoadingState
 import com.jbrunton.mymovies.R
 import com.jbrunton.mymovies.helpers.toVisibility
-import java.io.IOException
 
 class LoadingLayoutManager(root: View, val content: View) {
     val loadingIndicator = root.findViewById<View>(R.id.loading_indicator)
@@ -26,38 +25,49 @@ class LoadingLayoutManager(root: View, val content: View) {
     }
 
     fun <T>updateLayout(viewState: LoadingState<T>, onSuccess: (T) -> Unit) {
-        content.visibility = toVisibility(showContent(viewState))
-        loadingIndicator.visibility = toVisibility(showLoadingIndicator(viewState))
-        errorCase.visibility = toVisibility(showErrorCase(viewState))
+        onResetLayout()
+
         when (viewState) {
-            is LoadingState.Failure -> {
-                val cachedValue = viewState.cachedValue
-                if (cachedValue == null) {
-                    showErrorDetails(viewState.error)
-                } else {
-                    onSuccess(cachedValue)
-                }
-            }
             is LoadingState.Success -> {
+                content.visibility = View.VISIBLE
                 onSuccess(viewState.value)
+            }
+            is LoadingState.Loading -> {
+                onLoading(viewState, onSuccess)
+            }
+            is LoadingState.Failure -> {
+                onFailure(viewState, onSuccess)
             }
         }
     }
 
-    protected fun <T>showContent(viewState: LoadingState<T>): Boolean {
-        return viewState is LoadingState.Success
-                || viewState is LoadingState.Failure && viewState.cachedValue != null
+    protected fun onResetLayout() {
+        content.visibility = View.GONE
+        loadingIndicator.visibility = View.GONE
+        errorCase.visibility = View.GONE
     }
 
-    protected fun <T>showLoadingIndicator(viewState: LoadingState<T>): Boolean {
-        return viewState is LoadingState.Loading
+    protected fun <T>onLoading(viewState: LoadingState.Loading<T>, onSuccess: (T) -> Unit) {
+        val cachedValue = viewState.cachedValue
+        if (cachedValue == null) {
+            loadingIndicator.visibility = View.VISIBLE
+        } else {
+            content.visibility = View.VISIBLE
+            onSuccess(cachedValue)
+        }
     }
 
-    protected fun <T>showErrorCase(viewState: LoadingState<T>): Boolean {
-        return viewState is LoadingState.Failure && viewState.cachedValue == null
+    protected fun <T>onFailure(viewState: LoadingState.Failure<T>, onSuccess: (T) -> Unit) {
+        val cachedValue = viewState.cachedValue
+        if (cachedValue == null) {
+            onError(viewState.error)
+        } else {
+            content.visibility = View.VISIBLE
+            onSuccess(cachedValue)
+        }
     }
-
-    protected fun showErrorDetails(error: Throwable) {
+    
+    protected fun onError(error: Throwable) {
         if (error is LoadingViewStateError) {
             errorText.text = error.message
             errorTryAgain.visibility = toVisibility(error.allowRetry)
