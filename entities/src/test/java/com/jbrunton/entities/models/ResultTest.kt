21b7f82eb0d81a026e6ad11c4627336cce0217b6
John Brunton <jbrunton@zipcar.co.uk>
2018-11-25 11:13:21 +0000

    finish renaming tests, and some missing tests

--- entities/src/test/java/com/jbrunton/entities/models/ResultTest.kt
+++ entities/src/test/java/com/jbrunton/entities/models/ResultTest.kt
@@ -2,6 +2,7 @@ package com.jbrunton.entities.models
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.lang.NullPointerException
 import javax.xml.ws.http.HTTPException
 
 class ResultTest {
@@ -10,144 +11,169 @@ class ResultTest {
     val error = Throwable("error")
 
     @Test
+    fun returnsTheValue() {
+        assertThat(success(value).get()).isEqualTo(value)
+    }
+
+    @Test
+    fun returnsTheCachedValueIfLoading() {
+        assertThat(loading(value).get()).isEqualTo(value)
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun throwsIfLoadingAndNoCachedValue() {
+        loading(null).get()
+    }
+
+    @Test
+    fun returnsTheCachedValueIfFailure() {
+        assertThat(failure(IllegalStateException(), value).get()).isEqualTo(value)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun throwsIfFailureAndNoCachedValue() {
+        failure(IllegalStateException(), null).get()
+    }
+
+    @Test
     fun convertsSuccessToValueOrNull() {
-        assertThat(successState(value).getOrNull()).isEqualTo(value)
+        assertThat(success(value).getOrNull()).isEqualTo(value)
     }
 
     @Test
     fun convertsLoadingToValueOrNull() {
-        assertThat(loadingState(cachedValue).getOrNull()).isEqualTo(cachedValue)
-        assertThat(loadingState(null).getOrNull()).isNull()
+        assertThat(loading(cachedValue).getOrNull()).isEqualTo(cachedValue)
+        assertThat(loading(null).getOrNull()).isNull()
     }
 
     @Test
     fun convertsFailureToValueOrNull() {
-        assertThat(failureState(error, cachedValue).getOrNull()).isEqualTo(cachedValue)
-        assertThat(failureState(error,null).getOrNull()).isNull()
+        assertThat(failure(error, cachedValue).getOrNull()).isEqualTo(cachedValue)
+        assertThat(failure(error,null).getOrNull()).isNull()
     }
 
     @Test
-    fun transformsSuccessfulStates() {
-        val state = successState(2).map { it * 2 }
-        assertThat(state.get()).isEqualTo(4)
+    fun transformsSuccessfulResults() {
+        val result = success(2).map { it * 2 }
+        assertThat(result.get()).isEqualTo(4)
     }
 
     @Test
-    fun transformsLoadingStates() {
-        val state = loadingState(2).map { it * 2 }
-        assertThat(state.get()).isEqualTo(4)
+    fun transformsLoadingResults() {
+        val result = loading(2).map { it * 2 }
+        assertThat(result.get()).isEqualTo(4)
     }
 
     @Test
-    fun transformsFailureStates() {
-        val state = failureState(error, 2).map { it * 2 }
-        assertThat(state.get()).isEqualTo(4)
+    fun transformsFailureResults() {
+        val result = failure(error, 2).map { it * 2 }
+        assertThat(result.get()).isEqualTo(4)
     }
 
     @Test
-    fun transformsOnlySuccessStates() {
-        val state = successState(1).onSuccess {
+    fun transformsOnlySuccessResults() {
+        val result = success(1).onSuccess {
             Result.Success(it.value * 2)
         }
-        assertThat(state).isEqualTo(Result.Success(2))
+        assertThat(result).isEqualTo(Result.Success(2))
     }
 
     @Test
-    fun doesNotTransformOtherStatesWhenExpectingSuccess() {
-        loadingState(null).onSuccess { throw IllegalStateException() }
-        failureState(error,null).onSuccess { throw IllegalStateException() }
+    fun doesNotTransformOtherResultsWhenExpectingSuccess() {
+        loading(null).onSuccess { throw IllegalStateException() }
+        failure(error,null).onSuccess { throw IllegalStateException() }
     }
 
     @Test
-    fun transformsOnlyLoadingStates() {
-        val state = loadingState(1).onLoading {
+    fun transformsOnlyLoadingResults() {
+        val result = loading(1).onLoading {
             Result.Success(it.get() * 2)
         }
-        assertThat(state).isEqualTo(Result.Success(2))
+        assertThat(result).isEqualTo(Result.Success(2))
     }
 
     @Test
-    fun doesNotTransformOtherStatesWhenExpectingLoading() {
-        successState(value).onLoading { throw IllegalStateException() }
-        failureState(error,null).onLoading { throw IllegalStateException() }
+    fun doesNotTransformOtherResultsWhenExpectingLoading() {
+        success(value).onLoading { throw IllegalStateException() }
+        failure(error,null).onLoading { throw IllegalStateException() }
     }
 
     @Test
-    fun transformsOnlyFailureStates() {
-        val state = failureState(error,1).onFailure {
+    fun transformsOnlyFailureResults() {
+        val result = failure(error,1).onFailure {
             Result.Success(it.get() * 2)
         }
-        assertThat(state).isEqualTo(Result.Success(2))
+        assertThat(result).isEqualTo(Result.Success(2))
     }
 
     @Test
-    fun doesNotTransformOtherStatesWhenExpectingFailure() {
-        successState(value).onFailure { throw IllegalStateException() }
-        loadingState(value).onFailure { throw IllegalStateException() }
+    fun doesNotTransformOtherResultsWhenExpectingFailure() {
+        success(value).onFailure { throw IllegalStateException() }
+        loading(value).onFailure { throw IllegalStateException() }
     }
 
     @Test
     fun transformsErrors() {
-        val state = failureState(HTTPException(401), 1).onError(HTTPException::class) {
+        val result = failure(HTTPException(401), 1).onError(HTTPException::class) {
             map { Result.Success(it.get() * 2) }
         }
-        assertThat(state).isEqualTo(Result.Success(2))
+        assertThat(result).isEqualTo(Result.Success(2))
     }
 
     @Test
     fun transformsSpecificErrors() {
-        val failureState = failureState(HTTPException(401), 1)
+        val result = failure(HTTPException(401), 1)
 
-        val transformedState = failureState.onError(HTTPException::class) {
+        val transformedResult = result.onError(HTTPException::class) {
             map { Result.Success(it.get() * 2) } whenever { it.statusCode == 401 }
         }
-        val otherState = failureState.onError(HTTPException::class) {
+        val otherResult = result.onError(HTTPException::class) {
             map { Result.Success(it.get() * 2) } whenever { it.statusCode == 400 }
         }
 
-        assertThat(transformedState).isEqualTo(Result.Success(2))
-        assertThat(otherState).isEqualTo(failureState)
+        assertThat(transformedResult).isEqualTo(Result.Success(2))
+        assertThat(otherResult).isEqualTo(result)
     }
 
     @Test
     fun zipsSuccesses() {
-        val state = successState(2).zipWith(successState(3)) { x, y -> x * y }
-        assertThat(state).isEqualTo(Result.Success(6))
+        val result = success(2).zipWith(success(3)) { x, y -> x * y }
+        assertThat(result).isEqualTo(Result.Success(6))
     }
 
     @Test
     fun zipsLeftFailures() {
-        val state = failureState(error, 2).zipWith(successState(3)) { x, y -> x * y }
-        assertThat(state).isEqualTo(Result.Failure(error, 6))
+        val result = failure(error, 2).zipWith(success(3)) { x, y -> x * y }
+        assertThat(result).isEqualTo(Result.Failure(error, 6))
     }
 
     @Test
     fun zipsRightFailures() {
-        val state = successState(3).zipWith(failureState(error, 2)) { x, y -> x * y }
-        assertThat(state).isEqualTo(Result.Failure(error, 6))
+        val result = success(3).zipWith(failure(error, 2)) { x, y -> x * y }
+        assertThat(result).isEqualTo(Result.Failure(error, 6))
     }
 
     @Test
-    fun zipsLeftLoadingStates() {
-        val state = loadingState(2).zipWith(successState(3)) { x, y -> x * y }
-        assertThat(state).isEqualTo(Result.Loading(6))
+    fun zipsLeftLoadingResults() {
+        val result = loading(2).zipWith(success(3)) { x, y -> x * y }
+        assertThat(result).isEqualTo(Result.Loading(6))
     }
 
     @Test
-    fun zipsRightLoadingStates() {
-        val state = successState(2).zipWith(loadingState(3)) { x, y -> x * y }
-        assertThat(state).isEqualTo(Result.Loading(6))
+    fun zipsRightLoadingResults() {
+        val result = success(2).zipWith(loading(3)) { x, y -> x * y }
+        assertThat(result).isEqualTo(Result.Loading(6))
     }
 
-    private fun successState(value: Int): Result<Int> {
+    private fun success(value: Int): Result<Int> {
         return Result.Success(value)
     }
 
-    private fun loadingState(cachedValue: Int?): Result<Int> {
+    private fun loading(cachedValue: Int?): Result<Int> {
         return Result.Loading(cachedValue)
     }
 
-    private fun failureState(error: Throwable, cachedValue: Int?): Result<Int> {
+    private fun failure(error: Throwable, cachedValue: Int?): Result<Int> {
         return Result.Failure(error, cachedValue)
     }
 }
