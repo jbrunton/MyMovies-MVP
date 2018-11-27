a80c43146ef791fe9c22708ad0bdaaffe54b425f
John Brunton <jbrunton@zipcar.co.uk>
2018-11-27 22:02:58 +0000

    remove di package

--- app/src/main/java/com/jbrunton/mymovies/di/ActivityModule.kt
+++ app/src/main/java/com/jbrunton/mymovies/di/ActivityModule.kt
@@ -1,6 +1,7 @@
 package com.jbrunton.mymovies.di
 
 import androidx.fragment.app.FragmentActivity
+import com.jbrunton.inject.module
 import com.jbrunton.mymovies.nav.Navigator
 
 fun ActivityModule(activity: FragmentActivity) = module {
