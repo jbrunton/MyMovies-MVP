5f44f6f10c05f0c64d88b40f27ab01dfbda57aa3
John Brunton <jbrunton@zipcar.com>
2018-12-08 19:40:19 +0000

    refactor DebounceTextWatcher

--- app/src/main/java/com/jbrunton/mymovies/ui/shared/DebounceTextWatcher.kt
+++ app/src/main/java/com/jbrunton/mymovies/ui/shared/DebounceTextWatcher.kt
@@ -13,29 +13,33 @@ class DebounceTextWatcher(
         val action: (String) -> Unit
 ) : TextWatcher, CoroutineScope {
     private var previousValue = ""
+    private var newValue = ""
 
     companion object {
         val Delay: Long = 300
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        val newValue = s.toString().trim()
-        if (newValue == previousValue)
-            return
-
-        previousValue = newValue
-
-        launch {
-            delay(Delay)
-            if (newValue != previousValue)
-                return@launch
-
-            action(newValue)
+        newValue = s.toString().trim()
+        if (newValue != previousValue) {
+            previousValue = newValue
+            waitAndCheck()
         }
     }
 
     override fun afterTextChanged(s: Editable?) {}
     override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+    private fun waitAndCheck() {
+        launch {
+            delay(Delay)
+            if (newValue == previousValue) {
+                action(newValue)
+            } else {
+                return@launch
+            }
+        }
+    }
 }
 
 fun EditText.onTextChanged(context: CoroutineContext, action: (String) -> Unit): Unit =
