fd3a6e450ce19c43ec3f2c6b25ebcc0bcb052a12
John Brunton <jbrunton@zipcar.com>
2019-02-11 22:50:04 +0000

    replace SearchViewModelTest with SearchUseCaseTest

--- app/src/test/java/com/jbrunton/mymovies/ui/search/SearchUseCaseTest.kt
+++ app/src/test/java/com/jbrunton/mymovies/ui/search/SearchUseCaseTest.kt
@@ -10,7 +10,8 @@ import com.jbrunton.mymovies.fixtures.TestSchedulerRule
 import com.jbrunton.mymovies.ui.movies.MovieViewState
 import com.jbrunton.mymovies.ui.shared.LoadingViewState
 import com.jbrunton.mymovies.ui.shared.networkError
-import org.assertj.core.api.Assertions.assertThat
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subscribers.TestSubscriber
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -18,7 +19,7 @@ import org.mockito.Mockito
 import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
-class SearchViewModelTest {
+class SearchUseCaseTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
@@ -28,6 +29,8 @@ class SearchViewModelTest {
     private lateinit var repository: MoviesRepository
     private lateinit var useCase: SearchUseCase
     private lateinit var preferences: ApplicationPreferences
+    private lateinit var subscriber: TestSubscriber<LoadingViewState<SearchViewState>>
+    private lateinit var queries: PublishSubject<String>
     private val movieFactory = MovieFactory()
     private val MOVIE = movieFactory.create()
     private val NETWORK_ERROR = SocketTimeoutException()
@@ -36,36 +39,35 @@ class SearchViewModelTest {
     private val NETWORK_FAILURE_VIEW_STATE = LoadingViewState.failure(networkError(), MovieViewState.Empty)
     private val LOADING_VIEW_STATE = AsyncResult.Loading<MovieViewState>()
 
-    private lateinit var viewModel: SearchViewModel
-
     @Before
     fun setUp() {
         repository = Mockito.mock(MoviesRepository::class.java)
         useCase = SearchUseCase(repository)
         preferences = Mockito.mock(ApplicationPreferences::class.java)
-        viewModel = SearchViewModel(useCase)
         RepositoryFixtures.stubSearch(repository, "Star").toReturnDelayed(listOf(MOVIE), 1)
 
-        viewModel.start()
+        queries = PublishSubject.create()
+        subscriber = TestSubscriber.create()
+        useCase.start(queries).subscribe { subscriber.onNext(it) }
         schedulerRule.TEST_SCHEDULER.triggerActions()
     }
 
     @Test
     fun startsWithEmptyState() {
-        assertThat(viewModel.viewState.value).isEqualTo(SearchViewStateFactory.EmptyState)
+        subscriber.assertValue(SearchViewStateFactory.EmptyState)
     }
 
     @Test
     fun showsEmptyStateForEmptyQuery() {
-        viewModel.performSearch("")
+        queries.onNext("")
         schedulerRule.TEST_SCHEDULER.triggerActions()
-        assertThat(viewModel.viewState.value).isEqualTo(SearchViewStateFactory.EmptyState)
+        subscriber.assertValues(SearchViewStateFactory.EmptyState, SearchViewStateFactory.EmptyState)
     }
 
     @Test
     fun searchesForQuery() {
-        viewModel.performSearch("Star")
+        queries.onNext("Star")
         schedulerRule.TEST_SCHEDULER.advanceTimeBy(2, TimeUnit.SECONDS)
-        assertThat(viewModel.viewState.value).isEqualTo(SUCCESS_VIEW_STATE)
+        subscriber.assertValues(SearchViewStateFactory.EmptyState, SUCCESS_VIEW_STATE)
     }
 }
