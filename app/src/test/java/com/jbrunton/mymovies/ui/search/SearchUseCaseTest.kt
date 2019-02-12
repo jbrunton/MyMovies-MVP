1130a0833696d4d926694684c89d926f8e593867
John Brunton <jbrunton@zipcar.com>
2019-02-12 00:13:01 +0000

    simplify use case tests

--- app/src/test/java/com/jbrunton/mymovies/ui/search/SearchUseCaseTest.kt
+++ app/src/test/java/com/jbrunton/mymovies/ui/search/SearchUseCaseTest.kt
@@ -1,73 +1,69 @@
 package com.jbrunton.mymovies.ui.search
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.jbrunton.async.AsyncResult
+import com.jbrunton.entities.models.Movie
 import com.jbrunton.entities.repositories.MoviesRepository
 import com.jbrunton.fixtures.MovieFactory
-import com.jbrunton.mymovies.fixtures.RepositoryFixtures
-import com.jbrunton.mymovies.fixtures.TestSchedulerRule
+import com.jbrunton.mymovies.fixtures.InstantSchedulerRule
 import com.jbrunton.mymovies.ui.search.SearchViewStateFactory.Companion.EmptyState
 import com.jbrunton.mymovies.ui.shared.LoadingViewState
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
-import java.util.concurrent.TimeUnit
 
 class SearchUseCaseTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
     @get:Rule
-    var schedulerRule = TestSchedulerRule()
+    var schedulerRule = InstantSchedulerRule()
 
     private lateinit var repository: MoviesRepository
     private lateinit var useCase: SearchUseCase
     private lateinit var observer: TestObserver<LoadingViewState<SearchViewState>>
-    private lateinit var queries: PublishSubject<String>
+    private lateinit var searches: PublishSubject<String>
 
     private val movieFactory = MovieFactory()
+
     private val MOVIE = movieFactory.create()
-    private val SUCCESS_VIEW_STATE = LoadingViewState.success(SearchViewState.from(listOf(MOVIE)))
+    private val LOADING_RESULT = AsyncResult.loading<List<Movie>>(null)
+    private val SUCCESS_RESULT = AsyncResult.success(listOf(MOVIE))
+
+    private val LOADING_VIEW_STATE = SearchViewStateFactory.from(LOADING_RESULT)
+    private val SUCCESS_VIEW_STATE = SearchViewStateFactory.from(SUCCESS_RESULT)
 
     @Before
     fun setUp() {
         repository = Mockito.mock(MoviesRepository::class.java)
         useCase = SearchUseCase(repository)
-        RepositoryFixtures.stubSearch(repository, "Star").toReturnDelayed(listOf(MOVIE), 1)
 
-        queries = PublishSubject.create()
+        whenever(repository.searchMovies("Star"))
+                .thenReturn(Observable.just(LOADING_RESULT, SUCCESS_RESULT))
+
+        searches = PublishSubject.create()
+        observer = useCase.start(searches).test()
     }
 
     @Test
     fun startsWithEmptyState() {
-        startUseCase()
         observer.assertValue(EmptyState)
     }
 
     @Test
     fun showsEmptyStateForEmptyQuery() {
-        startUseCase()
-
-        queries.onNext("")
-
-        schedulerRule.TEST_SCHEDULER.triggerActions()
+        searches.onNext("")
         observer.assertValues(EmptyState, EmptyState)
     }
 
     @Test
     fun searchesForQuery() {
-        startUseCase()
-
-        queries.onNext("Star")
-
-        schedulerRule.TEST_SCHEDULER.advanceTimeBy(1, TimeUnit.SECONDS)
-        observer.assertValues(EmptyState, SUCCESS_VIEW_STATE)
-    }
-
-    private fun startUseCase() {
-        observer = useCase.start(queries).test()
-        schedulerRule.TEST_SCHEDULER.triggerActions()
+        searches.onNext("Star")
+        observer.assertValues(EmptyState, LOADING_VIEW_STATE, SUCCESS_VIEW_STATE)
     }
 }
