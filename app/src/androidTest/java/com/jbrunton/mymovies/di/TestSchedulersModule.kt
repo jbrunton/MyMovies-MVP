70c418c7b09f7fe38f2f29a683084cb25d69c1db
John Brunton <jbrunton@zipcar.com>
2019-02-16 15:55:59 +0000

    fix tests

--- app/src/androidTest/java/com/jbrunton/mymovies/di/TestSchedulersModule.kt
+++ app/src/androidTest/java/com/jbrunton/mymovies/di/TestSchedulersModule.kt
@@ -1,11 +1,12 @@
 package com.jbrunton.mymovies.di
 
+import com.jbrunton.entities.SchedulerFactory
+import com.jbrunton.fixtures.ImmediateSchedulerFactory
 import com.jbrunton.inject.module
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.test.TestCoroutineContext
 import kotlin.coroutines.CoroutineContext
 
 val TestSchedulersModule = module {
-    single { Schedulers.trampoline() }
     single { TestCoroutineContext() as CoroutineContext }
+    single { ImmediateSchedulerFactory() as SchedulerFactory }
 }
--- app/src/main/java/com/jbrunton/mymovies/di/SchedulersModule.kt
+++ app/src/main/java/com/jbrunton/mymovies/di/SchedulersModule.kt
@@ -3,12 +3,10 @@ package com.jbrunton.mymovies.di
 import com.jbrunton.entities.SchedulerFactory
 import com.jbrunton.inject.module
 import com.jbrunton.mymovies.ui.shared.AndroidSchedulerFactory
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
 val SchedulersModule = module {
-    single { Schedulers.computation() }
     single { Dispatchers.Main as CoroutineContext }
     single { AndroidSchedulerFactory() as SchedulerFactory }
 }
