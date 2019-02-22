7e10311f562011a6a0c5aa6be784abf1ab70fb0f
John Brunton <jbrunton@zipcar.com>
2019-02-22 22:04:53 +0000

    make DiscoverUseCase active

--- app-usecases/src/main/java/com/jbrunton/mymovies/usecases/discover/DiscoverUseCase.kt
+++ app-usecases/src/main/java/com/jbrunton/mymovies/usecases/discover/DiscoverUseCase.kt
@@ -1,18 +1,32 @@
 package com.jbrunton.mymovies.usecases.discover
 
 import com.jbrunton.async.AsyncResult
+import com.jbrunton.entities.SchedulerContext
 import com.jbrunton.entities.errors.handleNetworkErrors
-import com.jbrunton.entities.repositories.DataStream
 import com.jbrunton.entities.repositories.GenresRepository
 import com.jbrunton.entities.repositories.MoviesRepository
+import com.jbrunton.entities.subscribe
+import com.jbrunton.mymovies.usecases.BaseUseCase
 import io.reactivex.rxkotlin.Observables
+import io.reactivex.subjects.PublishSubject
 
 class DiscoverUseCase(
         val movies: MoviesRepository,
         val genres: GenresRepository
-) {
-    fun reduce(): DataStream<DiscoverState> {
-        return Observables.zip(
+) : BaseUseCase() {
+    val state = PublishSubject.create<AsyncResult<DiscoverState>>()
+
+    override fun start(schedulerContext: SchedulerContext) {
+        super.start(schedulerContext)
+        load()
+    }
+
+    fun retry() {
+        load()
+    }
+
+    private fun load() {
+        val stream = Observables.zip(
                 movies.nowPlaying(),
                 movies.popular(),
                 genres.genres()
@@ -20,5 +34,6 @@ class DiscoverUseCase(
             AsyncResult.zip(nowPlaying, popular, genres, ::DiscoverState)
                     .handleNetworkErrors()
         }
+        subscribe(stream, state::onNext)
     }
 }
