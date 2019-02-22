63723d515ad00f4705937f38ef7341ddd4ed3105
John Brunton <jbrunton@zipcar.com>
2019-02-22 22:54:00 +0000

    make favorites usecase active

--- app-usecases/src/main/java/com/jbrunton/mymovies/usecases/favorites/FavoritesUseCase.kt
+++ app-usecases/src/main/java/com/jbrunton/mymovies/usecases/favorites/FavoritesUseCase.kt
@@ -1,24 +1,37 @@
 package com.jbrunton.mymovies.usecases.favorites
 
 import com.jbrunton.async.AsyncResult
+import com.jbrunton.entities.SchedulerContext
 import com.jbrunton.entities.errors.doOnNetworkError
 import com.jbrunton.entities.errors.handleNetworkErrors
-import com.jbrunton.entities.repositories.DataStream
 import com.jbrunton.entities.repositories.MoviesRepository
+import com.jbrunton.entities.subscribe
+import com.jbrunton.mymovies.usecases.BaseUseCase
 import com.jbrunton.mymovies.usecases.search.SearchState
 import io.reactivex.subjects.PublishSubject
 
 class FavoritesUseCase(
         val movies: MoviesRepository
-) {
+) : BaseUseCase() {
     val retrySnackbar = PublishSubject.create<Unit>()
+    val favorites = PublishSubject.create<AsyncResult<SearchState>>()
 
-    fun movies(): DataStream<SearchState> {
-        return movies.favorites().map {
+    override fun start(schedulerContext: SchedulerContext) {
+        super.start(schedulerContext)
+        loadFavorites()
+    }
+
+    fun retry() {
+        loadFavorites()
+    }
+
+    private fun loadFavorites() {
+        val favorites = movies.favorites().map {
             SearchState.from(it)
                     .handleNetworkErrors()
                     .doOnNetworkError(this::showSnackbarIfCachedValue)
         }
+        subscribe(favorites) { this.favorites.onNext(it) }
     }
 
     private fun showSnackbarIfCachedValue(failure: AsyncResult.Failure<SearchState>) {
--- app/src/main/java/com/jbrunton/mymovies/ui/account/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/jbrunton/mymovies/ui/account/favorites/FavoritesViewModel.kt
@@ -12,19 +12,16 @@ class FavoritesViewModel(container: Container) : BaseLoadingViewModel<SearchView
     val useCase: FavoritesUseCase by inject()
 
     override fun start() {
-        loadFavorites()
         subscribe(useCase.retrySnackbar) {
             snackbar.postValue(NetworkErrorSnackbar(retry = true))
         }
+        subscribe(useCase.favorites) {
+            viewState.postValue(SearchViewStateFactory.from(it))
+        }
+        useCase.start(schedulerContext)
     }
 
     override fun retry() {
-        loadFavorites()
-    }
-
-    private fun loadFavorites() {
-        subscribe(useCase.movies()) {
-            viewState.postValue(SearchViewStateFactory.from(it))
-        }
+        useCase.retry()
     }
 }
