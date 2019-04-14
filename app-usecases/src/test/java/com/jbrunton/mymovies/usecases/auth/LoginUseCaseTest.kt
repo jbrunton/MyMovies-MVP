b45cf2e619e2d4ccc72b417fa9dac103f194a2aa
John Brunton <jbrunton@zipcar.com>
2019-04-14 15:54:10 +0100

    fix LoginUseCaseTest

--- app-usecases/src/test/java/com/jbrunton/mymovies/usecases/auth/LoginUseCaseTest.kt
+++ app-usecases/src/test/java/com/jbrunton/mymovies/usecases/auth/LoginUseCaseTest.kt
@@ -1,15 +1,12 @@
 package com.jbrunton.mymovies.usecases.auth
 
 import com.jbrunton.async.AsyncResult
-import com.jbrunton.entities.SchedulerContext
 import com.jbrunton.entities.models.AuthSession
 import com.jbrunton.entities.repositories.AccountRepository
-import com.jbrunton.fixtures.ImmediateSchedulerFactory
 import com.jbrunton.fixtures.NetworkErrorFixtures.httpErrorResult
 import com.jbrunton.fixtures.NetworkErrorFixtures.networkErrorResult
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
-import io.reactivex.observers.TestObserver
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
@@ -17,10 +14,6 @@ import org.mockito.Mockito
 class LoginUseCaseTest {
     private lateinit var repository: AccountRepository
     private lateinit var useCase: LoginUseCase
-    private lateinit var stateObserver: TestObserver<AsyncResult<LoginState>>
-    private lateinit var successObserver: TestObserver<AuthSession>
-    private lateinit var failureObserver: TestObserver<String>
-    private lateinit var networkErrorSnackbarObserver: TestObserver<Unit>
 
     private val AUTH_SESSION = AuthSession("1234")
     private val LOADING_RESULT = AsyncResult.loading<AuthSession>(null)
@@ -30,7 +23,9 @@ class LoginUseCaseTest {
     private val NETWORK_ERROR_RESULT = networkErrorResult<AuthSession>()
 
     private val LOADING_STATE = AsyncResult.loading(null)
-    private val SUCCESS_STATE = AsyncResult.success(LoginState.Valid)
+    private val SUCCESS_STATE = AsyncResult.success(LoginState.SignedIn(AUTH_SESSION))
+    private val NETWORK_ERROR_STATE = AsyncResult.failure(NETWORK_ERROR_RESULT.error, null)
+    private val AUTH_FAILURE_STATE = AsyncResult.failure(AUTH_FAILURE_RESULT.error, null)
 
     private val INVALID_USERNAME_STATE = AsyncResult.success(LoginState.Invalid(requiresUsername = true, requiresPassword = false))
     private val INVALID_PASSWORD_STATE = AsyncResult.success(LoginState.Invalid(requiresUsername = false, requiresPassword = true))
@@ -42,55 +37,39 @@ class LoginUseCaseTest {
     fun setUp() {
         repository = Mockito.mock(AccountRepository::class.java)
         useCase = LoginUseCase(repository)
-
-        successObserver = useCase.loginSuccessful.test()
-        failureObserver = useCase.loginFailure.test()
-        networkErrorSnackbarObserver = useCase.networkErrorSnackbar.test()
-        stateObserver = useCase.state.test()
-
-        useCase.start(SchedulerContext(ImmediateSchedulerFactory()))
     }
 
     @Test
     fun testSuccess() {
         stubLoginToReturn(SUCCESS_RESULT)
-
-        useCase.login(USERNAME, PASSWORD)
-
-        stateObserver.assertValues(LOADING_STATE, SUCCESS_STATE)
-        successObserver.assertValue(AUTH_SESSION)
+        val observer = useCase.login(USERNAME, PASSWORD).test()
+        observer.assertValues(LOADING_STATE, SUCCESS_STATE)
     }
 
     @Test
     fun testAuthFailure() {
         stubLoginToReturn(AUTH_FAILURE_RESULT)
-
-        useCase.login(USERNAME, PASSWORD)
-
-        stateObserver.assertValues(LOADING_STATE, SUCCESS_STATE)
-        failureObserver.assertValue(AUTH_FAILURE_MESSAGE)
+        val observer = useCase.login(USERNAME, PASSWORD).test()
+        observer.assertValues(LOADING_STATE, AUTH_FAILURE_STATE)
     }
 
     @Test
     fun testNetworkError() {
         stubLoginToReturn(NETWORK_ERROR_RESULT)
-
-        useCase.login(USERNAME, PASSWORD)
-
-        stateObserver.assertValues(LOADING_STATE, SUCCESS_STATE)
-        networkErrorSnackbarObserver.assertValue(Unit)
+        val observer = useCase.login(USERNAME, PASSWORD).test()
+        observer.assertValues(LOADING_STATE, NETWORK_ERROR_STATE)
     }
 
     @Test
     fun testInvalidUsername() {
-        useCase.login("", PASSWORD)
-        stateObserver.assertValues(INVALID_USERNAME_STATE)
+        useCase.login("", PASSWORD).test()
+                .assertValues(INVALID_USERNAME_STATE)
     }
 
     @Test
     fun testInvalidPassword() {
-        useCase.login(USERNAME, "")
-        stateObserver.assertValues(INVALID_PASSWORD_STATE)
+        useCase.login(USERNAME, "").test()
+                .assertValues(INVALID_PASSWORD_STATE)
     }
 
     private fun stubLoginToReturn(result: AsyncResult<AuthSession>) {
