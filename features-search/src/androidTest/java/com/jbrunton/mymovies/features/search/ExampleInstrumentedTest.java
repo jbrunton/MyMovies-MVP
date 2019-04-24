3072d6a821b76ac519ec6a43161207280b962d6d
John Brunton <jbrunton@zipcar.com>
2019-04-24 20:45:59 +0100

    add a couple of failing tests

--- features-search/src/androidTest/java/com/jbrunton/mymovies/features/search/ExampleInstrumentedTest.java
+++ features-search/src/androidTest/java/com/jbrunton/mymovies/features/search/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package com.jbrunton.mymovies.features.search;
 
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jbrunton.mymovies.features.search.test", appContext.getPackageName());
+        assertEquals("com.jbrunton.mymovies.features.search.test.foo", appContext.getPackageName());
     }
 }
